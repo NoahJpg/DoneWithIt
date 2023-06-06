5ea056f8134c632a41a85cc0aeb61851f6f77253
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-06-06 16:01:52 -0700

    add ViewImageScreen

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View 
+        style={{
+        backgroundColor: "dodgerblue",
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderColor: "royalblue",
+        borderRadius: 50,
+      }}
+      ></View>
+    </View>
   );
 }
