ed257486bec5b135cfba7c93de90f3c5bed6c972
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-06-08 16:33:16 -0700

    add messages screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { TextInput, View, Text } from "react-native";
 
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import Screen from "./app/components/Screen"
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+
   return (
-    <ListingDetailsScreen />
+    <MessagesScreen />
   );
 }
---
+++ app/components/Screen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Screen(props) {
+  
+}
+
+export default Screen;
